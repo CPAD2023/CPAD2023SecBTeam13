d6c8af16caf4446b6b537c0d410931c3cd0f577f
Shreesha KJ <shreeshakjbahuman@gmail.com>
2023-12-02 12:27:58 +0530

    Handle Search and Handle Location
    Created initial function to handle location calls and search calls.

---
+++ screens/HomeScreen.js
@@ -0,0 +1,53 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { MagnifyingGlassIcon, XMarkIcon } from 'react-native-heroicons/outline'
+import {  MapPinIcon } from 'react-native-heroicons/solid'
+import { debounce } from "lodash";
+import { theme } from '../theme';
+import { fetchLocations, fetchWeatherForecast } from '../api/weather';
+import * as Progress from 'react-native-progress';
+import { StatusBar } from 'expo-status-bar';
+import { getData, storeData } from '../utils/asyncStorage';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+export default function HomeScreen() {
+  const [showSearch, toggleSearch] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState({})
+
+
+  const handleSearch = search=>{
+    // console.log('value: ',search);
+    if(search && search.length>2)
+      fetchLocations({cityName: search}).then(data=>{
+        // console.log('got locations: ',data);
+        setLocations(data);
+      })
+  }
+
+  const handleLocation = loc=>{
+    setLoading(true);
+    toggleSearch(false);
+    setLocations([]);
+    fetchWeatherForecast({
+      cityName: loc.name,
+      days: '7'
+    }).then(data=>{
+      setLoading(false);
+      setWeather(data);
+      storeData('city',loc.name);
+    })
+  }
+
+  useEffect(()=>{
+    fetchMyWeatherData();
+  },[]);
+
+ 
+
+  const handleTextDebounce = useCallback(debounce(handleSearch, 1200), []);
+
+
+}
